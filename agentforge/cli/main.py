0efd748214b4b273b1a1843f45fcfdc3748635c6
faysel <fayselcode@gmail.com>
2026-05-21 11:46:56 +0300

    Add a simple evaluation logic

--- agentforge/cli/main.py
+++ agentforge/cli/main.py
@@ -15,6 +15,25 @@ def call_agent(endpoint: str, messages: list):
     return response.json()["response"]
 
 
+def evaluate(response: str, rules: list):
+    for rule in rules:
+        print(rule)
+
+        # RULE 1: must_not_contain
+        if rule["type"] == "must_not_contain":
+            for word in rule["value"]:
+                if word.lower() in response.lower():
+                    return False
+
+        # RULE 2: must_contain
+        if rule["type"] == "must_contain":
+            for word in rule["value"]:
+                if word.lower() not in response.lower():
+                    return False
+
+    return True
+
+
 @app.command()
 def test(file_path: str):
     # Load test file
@@ -22,15 +41,23 @@ def test(file_path: str):
     content = file.read()
     data = json.loads(content)
 
-    # Extract the conversation
+    # Extract the conversation and rules
     conversation = data["conversation"]
+    rules = data["rules"]
 
     url = "https://6a0ebe061736097c360a69e8.mockapi.io/chat/chat"
 
     reply = call_agent(url, conversation)
+    passed = evaluate(reply, rules)
 
-    print("\nAgent Reply:\n")
+    print("\nAgent response:\n")
     print(reply)
+
+    print("\nResult:\n")
+    if passed:
+        print("PASS")
+    else:
+        print("FAIL")
 
 
 if __name__ == "__main__":
